57f369ce8b499bf479160d35a44e4a10e1043297
Oscar Savolainen <oscar.savolainen@deeprender.com>
2024-02-09 18:48:22 +0000

    We do FX Graph Mode Quantization

--- Resnet/evaluate.py
+++ Resnet/evaluate.py
@@ -1,5 +1,4 @@
 import torch
-from model.resnet import resnet18
 
 def evaluate(model, device_str: str):
     # Download an example image from the pytorch website
@@ -45,49 +44,3 @@ def evaluate(model, device_str: str):
     top5_prob, top5_catid = torch.topk(probabilities, 5)
     for i in range(top5_prob.size(0)):
         print(categories[top5_catid[i]], top5_prob[i].item())
-
-model = resnet18(pretrained=True)
-# print(model)
-
-# Step 1: architecture changes
-# QuantStubs (we will do FloatFunctionals later)
-# Done
-
-# Step 2: fuse modules (recommended but not necessary)
-modules_to_list = model.modules_to_fuse()
-
-# It will keep Batchnorm
-model.eval()
-# fused_model = torch.ao.quantization.fuse_modules_qat(model, modules_to_list)
-
-# This will fuse BatchNorm weights into the preceding Conv
-fused_model = torch.ao.quantization.fuse_modules(model, modules_to_list)
-
-# Step 3: Assign qconfigs
-backend = 'qnnpack'
-qconfig = torch.quantization.get_default_qconfig(backend)
-torch.backends.quantized.engine = backend
-
-for name, module in fused_model.named_modules():
-    module.qconfig = qconfig
-
-# Step 4: Prepare for fake-quant
-fake_quant_model = torch.ao.quantization.prepare(fused_model)
-
-# Evaluate
-print('\noriginal')
-evaluate(model, 'cuda')
-print('\nfused')
-evaluate(fused_model, 'cuda')
-
-
-# Step 5: convert (true int8 model)
-fake_quant_model.to('cpu')
-converted_model = torch.quantization.convert(fake_quant_model)
-
-print('\nfake quant')
-evaluate(fake_quant_model, 'cuda')
-
-
-print('\nconverted')
-evaluate(converted_model, 'cpu')
---
+++ Resnet/qconfigs.py
@@ -0,0 +1,40 @@
+import torch
+import torch.quantization as tq
+from torch.ao.quantization.fake_quantize import FixedQParamsFakeQuantize
+from torch.ao.quantization._learnable_fake_quantize import (
+    _LearnableFakeQuantize as LearnableFakeQuantize,
+)
+
+learnable_act = lambda range : LearnableFakeQuantize.with_args(
+    observer=tq.HistogramObserver,
+    quant_min=0,
+    quant_max=255,
+    dtype=torch.quint8,
+    qscheme=torch.per_tensor_affine,
+    scale=range / 255.0,
+    zero_point=0.0,
+    use_grad_scaling=True,
+)
+
+learnable_weights = LearnableFakeQuantize.with_args(  # need to specify number of channels here
+    observer=tq.PerChannelMinMaxObserver,
+    quant_min=-128,
+    quant_max=127,
+    dtype=torch.qint8,
+    qscheme=torch.per_channel_symmetric,
+    scale=0.1,
+    zero_point=0.0,
+    use_grad_scaling=True,
+    # channel_len=channels,
+)
+
+fixed_act = lambda min , max : FixedQParamsFakeQuantize.with_args(
+    observer=torch.ao.quantization.observer.FixedQParamsObserver.with_args(
+        scale=(max - min) / 255.0,
+        zero_point=-min / ((max - min) / 255.0),  #  zero_point = -min / scale
+        quant_min=0,
+        quant_max=255,
+        dtype=torch.quint8,
+        qscheme=torch.per_tensor_affine,
+    ),
+)
