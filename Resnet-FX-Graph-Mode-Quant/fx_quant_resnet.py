0053bd7b787a4b8bdda13e340df7b76b8b4a1fd6
Oscar Savolainen <oscar.savolainen@hotmail.com>
2024-02-15 12:59:16 -0500

    Added examples, e.g. a generalised iterator pattern one can use to store attributes about the inputted tensor. Could use as the basis of analysis visulaitzation histograms.

--- Resnet-FX-Graph-Mode-Quant/fx_quant_resnet.py
+++ Resnet-FX-Graph-Mode-Quant/fx_quant_resnet.py
@@ -4,6 +4,7 @@ import torch.ao.quantization as taq
 from torch.ao.quantization.quantize_fx import prepare_fx, prepare_qat_fx
 from torch.ao.quantization.qconfig_mapping import QConfigMapping
 import torch.nn.quantized as nnq
+import torch.fx as fx
 
 from evaluate import evaluate
 from qconfigs import learnable_act, learnable_weights, fixed_act
@@ -77,10 +78,117 @@ evaluate(fake_quant_model, 'cpu')
 # Prints the graph as a table
 print("\nGraph as a Table:\n")
 fx_model.graph.print_tabular()
-xxx
+
 # Plots the graph
 # Need to install GraphViz and have it on PATH (or as a local PATH variable)
 #from torch.fx import passes
 #g = passes.graph_drawer.FxGraphDrawer(fx_model, 'fx-model')
 #with open("graph.svg", "wb") as f:
     #f.write(g.get_dot_graph().create_svg())
+
+# We will correct the graph to have fixedqparams on the input quantstub
+# NOTE: taken from https://pytorch.org/docs/stable/fx.html#graph-manipulation
+def transform(m: torch.nn.Module,
+              tracer_class : type = fx.Tracer) -> torch.nn.Module:
+    graph : fx.Graph = tracer_class().trace(m)
+    # FX represents its Graph as an ordered list of
+    # nodes, so we can iterate through them.
+    for node in graph.nodes:
+        # Checks if we're calling a function (i.e:
+        # torch.add)
+        if node.op == 'call_function':
+            # The target attribute is the function
+            # that call_function calls.
+            if node.target == torch.add:
+                node.target = torch.mul
+
+    graph.lint() # Does some checks to make sure the
+                 # Graph is well-formed.
+
+    return fx.GraphModule(m, graph)
+
+# Experiment with iterator pattern:
+# NOTE: taken from https://pytorch.org/docs/stable/fx.html#the-interpreter-pattern
+from torch.fx.node import Node
+from typing import Dict
+
+class GraphIteratorStorage:
+    """
+    A general Iterator over the graph. This class takes a `GraphModule`,
+    and a callable `storage` representing a function that will store some
+    attribute for each node when the `propagate` method is called.
+
+    Its `propagate` method executes the `GraphModule`
+    node-by-node with the given arguments, e.g. an example input tensor.
+    As each operation executes, the GraphIteratorStorage class stores
+    away the result of the callable for the output values of each operation on
+    the attributes of the operation's `Node`. For exmaple,
+    one could use a callable `store_shaped_dtype()` where:
+
+    ```
+    def store_shape_dtype(result):
+        if isinstance(result, torch.Tensor):
+            node.shape = result.shape
+            node.dtype = result.dtype
+    ```
+    This would store the `shape` and `dtype` of each operation on
+    its respective `Node`, for the given input to `propagate`.
+    """
+    def __init__(self, mod, storage):
+        self.mod = mod
+        self.graph = mod.graph
+        self.modules = dict(self.mod.named_modules())
+        self.storage = storage
+
+    def propagate(self, *args):
+        args_iter = iter(args)
+        env : Dict[str, Node] = {}
+
+        def load_arg(a):
+            return torch.fx.graph.map_arg(a, lambda n: env[n.name])
+
+        def fetch_attr(target : str):
+            target_atoms = target.split('.')
+            attr_itr = self.mod
+            for i, atom in enumerate(target_atoms):
+                if not hasattr(attr_itr, atom):
+                    raise RuntimeError(f"Node referenced nonexistant target {'.'.join(target_atoms[:i])}")
+                attr_itr = getattr(attr_itr, atom)
+            return attr_itr
+
+        for node in self.graph.nodes:
+            if node.op == 'placeholder':
+                result = next(args_iter)
+            elif node.op == 'get_attr':
+                result = fetch_attr(node.target)
+            elif node.op == 'call_function':
+                result = node.target(*load_arg(node.args), **load_arg(node.kwargs))
+            elif node.op == 'call_method':
+                self_obj, *args = load_arg(node.args)
+                kwargs = load_arg(node.kwargs)
+                result = getattr(self_obj, node.target)(*args, **kwargs)
+            elif node.op == 'call_module':
+                result = self.modules[node.target](*load_arg(node.args), **load_arg(node.kwargs))
+
+            # This is the only code specific to the `storage` function.
+            # you can delete this `if` branch and this becomes
+            # a generic GraphModule interpreter
+            self.storage(node, result)
+
+            env[node.name] = result
+        #return load_arg(self.graph.result)
+
+def store_shape_dtype(node, result):
+    """
+    Function that takes in the current node, and the tensor it is operating on (`result`)
+    and stores the shape and dtype of `result` on the node as attributes.
+    """
+    if isinstance(result, torch.Tensor):
+        node.shape = result.shape
+        node.dtype = result.dtype
+
+GraphIteratorStorage(fx_model, store_shape_dtype).propagate(example_inputs[0])
+for node in fx_model.graph.nodes:
+    print(node.name, node.shape, node.dtype)
+xxx
+
