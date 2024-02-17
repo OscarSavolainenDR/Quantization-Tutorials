4222b8e91683c3512191220c8b7cb8badd36992f
Oscar Savolainen <oscar.savolainen@hotmail.com>
2024-02-16 22:44:38 -0500

    Completing tut

--- Resnet-Eager-Mode-Quant/quant_resnet.py
+++ Resnet-Eager-Mode-Quant/quant_resnet.py
@@ -1,8 +1,9 @@
 import torch
 from model.resnet import resnet18
+from evaluate import evaluate
 
 model = resnet18(pretrained=True)
-print(model)
+#print(model)
 
 # Step 1: architecture changes
 # QuantStubs (we will do FloatFunctionals later)
@@ -19,13 +20,30 @@ model.eval()
 fused_model = torch.ao.quantization.fuse_modules(model, modules_to_list)
 
 # Step 3: Assign qconfigs
+backend = 'fbgemm'# if x86 else 'qnnpack'
+qconfig = torch.quantization.get_default_qconfig(backend)
+torch.backends.quantized.engine = backend
 
+fused_model.qconfig = qconfig
 
 # Step 4: Prepare for fake-quant
-
+fused_model.train()
+fake_quant_model = torch.quantization.prepare_qat(fused_model, inplace=False)
 
 # Step 5: convert (true int8 model)
+converted_model = torch.quantization.convert(fake_quant_model)
 
 
+print('\nfloat model')
+evaluate(model)
+
+print('\nfused model')
+evaluate(fused_model)
+
+print('\nfake quant model')
+evaluate(fake_quant_model)
+
+print('\nconverted model')
+evaluate(converted_model)
 import ipdb
 ipdb.set_trace()
